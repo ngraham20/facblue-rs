7f9f3afc9b3fe0136e39012d5df1dff511181e92
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-12-07 00:31:11 -0700

    removed encoding command arg

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,15 @@ mod blueprint_lib;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    decode: Option<String>,
-    #[arg(short, long)]
-    encode: Option<String>,
+    #[arg(short, long, required = true)]
+    file: String,
 }
 
 fn main() -> Result<(), Error> {
     let args =Args::parse();
-    if let Some(path) = args.decode {
-        let blueprint = fs::read_to_string(path).context("Failed to read file")?;
-        let bp = BP::decode(blueprint)?;
-        println!("{:?}", bp);
-    }
-
-    if let Some(path) = args.encode {
-        let json = fs::read_to_string(path).context("Failed to read file")?;
-    }
+    let blueprint = fs::read_to_string(args.file).context("Failed to read file")?;
+    let bp = BP::decode(blueprint)?;
+    println!("{:?}", bp);
 
     Ok(())
 }
