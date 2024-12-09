4069628628ec48036397ce2e2383d185044704dc
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-12-09 15:25:05 -0700

    re-designed wrap structs to be an enum instead

--- src/blueprint_lib/mod.rs
+++ src/blueprint_lib/mod.rs
@@ -10,11 +10,56 @@ mod blueprint;
 use blueprint::*;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct BP {
-    blueprint: Blueprint,
+#[serde(untagged)]
+pub enum BlueprintMeta {
+    Book{blueprint_book: BlueprintBook, index: Option<usize>},
+    Blueprint{blueprint: Blueprint, index: Option<usize>}
 }
 
-impl BP {
+impl BlueprintMeta {
+    pub fn decode(bp_string: String) -> Result<Self, Error> {
+        let bytes = general_purpose::STANDARD.decode(&bp_string[1..])?;
+        let mut deflater = ZlibDecoder::new(&bytes[..]);
+        let mut s = String::new();
+        deflater.read_to_string(&mut s).context("failed to read string")?;
+        println!("{}\n", s);
+        Ok(serde_json::from_str(&s).context("Failed to parse JSON")?)
+    }
+
+    pub fn parse_json(json_string: String) -> Result<Self, Error> {
+        Ok(serde_json::from_str(&json_string).context("Failed to parse JSON")?)
+    }
+
+    pub fn make_json(bp: &Self) -> Result<String, Error> {
+        Ok(serde_json::to_string(&bp)?)
+    }
+
+    pub fn encode(bp: &Self) -> Result<String, Error> {
+        let json_string = serde_json::to_string(&bp)?;
+        println!("{}", json_string);
+        let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+        encoder.write_all(json_string.as_bytes())?;
+        let compressed = encoder.finish()?;
+        let bp_string = general_purpose::STANDARD.encode(compressed);
+        Ok(format!("0{}", bp_string))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct BlueprintBook {
+    blueprints: Vec<BlueprintMeta>,
+    active_index: usize,
+    item: String,
+    label: String,
+    version: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct BlueprintWrap {
+    pub blueprint: Blueprint,
+}
+
+impl BlueprintWrap {
     pub fn decode(bp_string: String) -> Result<Self, Error> {
         let bytes = general_purpose::STANDARD.decode(&bp_string[1..])?;
         let mut deflater = ZlibDecoder::new(&bytes[..]);
@@ -23,7 +68,7 @@ impl BP {
         Ok(serde_json::from_str(&s).context("Failed to parse JSON")?)
     }
 
-    pub fn encode(bp: &BP) -> Result<String, Error> {
+    pub fn encode(bp: &BlueprintWrap) -> Result<String, Error> {
         let json_string = serde_json::to_string(&bp)?;
         println!("{}", json_string);
         let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
@@ -31,6 +76,42 @@ impl BP {
         let compressed = encoder.finish()?;
         let bp_string = general_purpose::STANDARD.encode(compressed);
         Ok(format!("0{}", bp_string))
+    }
+
+    pub fn test_blueprint() -> Result<String, Error> {
+        let bp: BlueprintWrap = BlueprintWrap {
+            blueprint: Blueprint::new(String::from("blueprint"), vec![Icon {
+                index: 1,
+                signal: Signal {
+                    name: String::from("signal-info"),
+                    signal_type: Some(SignalType::Virtual),
+                }
+            }], 562949954928640)
+            .with_entities(vec![
+                Entity {
+                    control_behavior: ControlBehavior {
+                        sections: Sections {
+                            sections: vec![
+                                Section::new(1, vec![
+                                    LogisticFilter::new(1, String::from("signal-0"), 1, Quality::Normal, Comparator::EqualTo)
+                                    .with_signal_type(SignalType::Virtual),
+                                    LogisticFilter::new(2, String::from("signal-C"), 1, Quality::Normal, Comparator::EqualTo)
+                                    .with_signal_type(SignalType::Virtual),
+                                    LogisticFilter::new(3, String::from("signal-Z"), 1, Quality::Normal, Comparator::EqualTo)
+                                    .with_signal_type(SignalType::Virtual),
+                                    LogisticFilter::new(4, String::from("signal-dot"), 1, Quality::Normal, Comparator::EqualTo)
+                                    .with_signal_type(SignalType::Virtual),
+                                ],)
+                            ]
+                        },
+                    },
+                    entity_number: 1,
+                    name: String::from("constant-combinator"),
+                    position: Position::from_xy(4.5, 23.5),
+                }
+            ]),
+        };
+        Ok(BlueprintWrap::encode(&bp)?)
     }
 }
 
@@ -42,7 +123,7 @@ mod tests {
     #[test]
     fn test_decode() -> Result<(), Error> {
         let bp_string = String::from("0eNqdksFuwjAMht/F54CgpGiNtBNvsQmhtITNUut0qVutqvLuOEUwbZdqKJfkt/39TpwJyrp3bUBiMBNg5akD8z5Bhx9k66Tx2DowMGDgXhQFZJsk3DJWSBcPUQHS2X2D2cajAkeMjO5Gmg/jifqmdEESHoDkxZZ4VfmmRLLsg9Bb30mtp2QtPL3OFYxgst06Fxep4eDrU+k+7YBSIFmdq1JB93svzveWFFywZhf+qgs324jyJQHpXkTyoZmTpNvWhrlbA6+z0KfX20b1gGeL8MPz8N0i/O15uF6Enz3/F3+UFdPHQHaNBH/+nIJB5jKPO99nhS6KXBfZy15vYrwC7F3c4g==");
-        let bp: BP = BP {
+        let bp: BlueprintWrap = BlueprintWrap {
             blueprint: Blueprint::new(String::from("blueprint"), vec![Icon {
                 index: 1,
                 signal: Signal {
@@ -74,13 +155,13 @@ mod tests {
                 }
             ]),
         };
-        assert_eq!(BP::decode(bp_string).expect("Failed to decode bp_string"), bp);
+        assert_eq!(BlueprintWrap::decode(bp_string).expect("Failed to decode bp_string"), bp);
         Ok(())
     }
     
     #[test]
     fn test_encode_decode() -> Result<(), Error> {
-        let bp: BP = BP {
+        let bp: BlueprintWrap = BlueprintWrap {
             blueprint: Blueprint::new(String::from("blueprint"), vec![Icon {
                 index: 1,
                 signal: Signal {
@@ -112,8 +193,8 @@ mod tests {
                 }
             ]),
         };
-        let encoded = BP::encode(&bp)?;
-        let decoded: BP = BP::decode(encoded)?;
+        let encoded = BlueprintWrap::encode(&bp)?;
+        let decoded: BlueprintWrap = BlueprintWrap::decode(encoded)?;
         assert_eq!(decoded, bp);
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use anyhow::{Context, Error};
-use blueprint_lib::BP;
+use blueprint_lib::BlueprintMeta;
 use clap::{arg, Parser};
 
 mod blueprint_lib;
@@ -8,16 +8,25 @@ mod blueprint_lib;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, required = true)]
-    file: String,
+    #[arg(short, long)]
+    file: Option<String>,
+    #[arg(short, long)]
+    json: Option<String>,
 }
 
 fn main() -> Result<(), Error> {
     let args =Args::parse();
-    let blueprint = fs::read_to_string(args.file).context("Failed to read file")?;
-    let bp = BP::decode(blueprint)?;
-    println!("{:?}", bp);
-
+    if let Some(path) = args.file {
+        let blueprint = fs::read_to_string(path).context("Failed to read file")?;
+        let bp = BlueprintMeta::decode(blueprint)?;
+        println!("{:?}", bp);
+    }
+    if let Some(path) = args.json {
+        let json = fs::read_to_string(path).context("Failed to read file")?;
+        let bp = BlueprintMeta::parse_json(json)?;
+        println!("{:?}\n", bp);
+        println!("{}", BlueprintMeta::make_json(&bp)?);
+    }
     Ok(())
 }
 
